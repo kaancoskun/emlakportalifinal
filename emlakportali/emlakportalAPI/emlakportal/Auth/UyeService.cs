6ab6f0d0b55356f63a9e894aa0291982519eb560
kaancoskun <kaancobusiness@gmail.com>
2023-06-16 16:53:44 +0300

    emlak portalı
    internet programcılığı 2 final

---
+++ emlakportali/emlakportalAPI/emlakportal/Auth/UyeService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using emlakportal.ViewModel;
+using emlakportal.Models;
+
+namespace emlakportal.Auth
+{
+    public class UyeService
+    {
+        emlakportaliDB01Entities db = new emlakportaliDB01Entities();
+
+        public UyeModel UyeOturumAc(string kadi, string parola)
+        {
+            UyeModel uye = db.Uye.Where(s => s.KullaniciAdi == kadi && s.Sifre == parola).Select(x => new UyeModel()
+            {
+                UyeId = x.UyeId,
+                AdSoyad = x.AdSoyad,
+                Email = x.Email,
+                KullaniciAdi = x.KullaniciAdi,
+                Foto = x.Foto,
+                Sifre = x.Sifre,
+                UyeAdmin = x.UyeAdmin
+            }).SingleOrDefault();
+            return uye;
+        }
+
+      }
+   }
---
+++ emlakportali/emlakportalAPI/emlakportal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="emlakportal.WebApiApplication" Language="C#" %>
